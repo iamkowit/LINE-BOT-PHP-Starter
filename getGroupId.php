a2469325ab2d5b6d59cdeea0c1bc1ad5abcd28be
iamkowit <30616703+iamkowit@users.noreply.github.com>
2017-08-01 14:51:19 +0700

    Update getGroupId.php

--- getGroupId.php
+++ getGroupId.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'TOW4YNTmRkyAclv+CqHOQZWwTC9pYN+TeljKbAIYLL+rLp9szOKEc79EM78Ug1eiR988A4NB1gf5KLIqyqBCG4VdGpGjm23nnh1cX9TE9HwQgvw6tz75lrwQIiKaNuocPtQq2jFQBBr5+IVVXi/QpAdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v2/bot/profile/u2c0f0963f7e113503bd36a11be17c21f';
+$url = 'https://api.line.me/v2/bot/profile/1519664504';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
