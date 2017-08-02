ef2d1ce3e142d79b82575acad2f18cef89a0145a
iamkowit <30616703+iamkowit@users.noreply.github.com>
2017-08-02 09:27:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,10 +20,20 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [ 
+			$messages = [ {
 				'type' => 'text',
-				'text' => $grp
-			];
+				'text' => $text 
+			},
+				     {
+     				'type' => 'text',
+				'text' => $room 
+				     },
+				     {
+     				'type' => 'text',
+				'text' => $group 
+				     }
+
+				    ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
